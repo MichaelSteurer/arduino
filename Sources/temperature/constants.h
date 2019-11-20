2ebb11b19060abf82333bab57fc9a8ad739e6c93
Michael Steurer <m.steurer@beaconinside.com>
2019-11-20 14:16:46 +0100

    Temperature sensor type + http

--- Sources/temperature/constants.h
+++ Sources/temperature/constants.h
@@ -4,19 +4,13 @@
 #include <pins_arduino.h>
 #include <c_types.h>
 
+
 const char* WIFI_SSID = "";
 const char* WIFI_PASSWORD = "";
 
-const char* MQTT_SERVER = "";
-const int MQTT_PORT = 1883;
-const char* MQTT_ID = "";
-const char* MQTT_USER = NULL;
-const char* MQTT_PASSWORD = NULL;
+const char* REQUEST_URL = "";
 
-const char* TOPIC = "";
-
-uint8_t DHTPIN = D5;
-uint8_t DHTTYPE = DHT22;
+uint8_t DATA_PIN = D1;
 
 const bool DEBUG = true;
 
--- Sources/temperature/temperature.ino
+++ Sources/temperature/temperature.ino
@@ -1,6 +1,6 @@
-#include <DHT.h>
-#include <ESP8266WiFi.h>
-#include <PubSubClient.h>
+// #include <DHT.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #include <CustomNetwork.h>
 #include <CustomHelpers.h>
@@ -9,8 +9,10 @@
 
 
 WiFiClient wifi_client_;
-PubSubClient mqtt_client_(wifi_client_);
-DHT dht_(DHTPIN, DHTTYPE);
+
+// DHT dht_(DATA_PIN, DHT22);
+OneWire ourWire(DATA_PIN);
+DallasTemperature sensors(&ourWire);
 
 CustomLogger logger_(DEBUG);
 CustomNetwork custom_network_(DEBUG);
@@ -20,32 +22,46 @@ CustomNetwork custom_network_(DEBUG);
 void setup()
 {
     custom_network_.wifiSetup(WIFI_SSID, WIFI_PASSWORD);
-    custom_network_.wifiConnect();
-
-    custom_network_.mqttSetup(&mqtt_client_, MQTT_SERVER, MQTT_PORT, MQTT_ID, MQTT_USER, MQTT_PASSWORD);
-    custom_network_.mqttConnect();
-
-    dht_.begin();
+    // dht_.begin();
 }
 
 
 //-----------------------------------------------------------------------------
-void loop()
-{
-    if (!custom_network_.mqttConnected())
-        custom_network_.mqttConnect();
-    custom_network_.mqttLoop();
+void loop() {
+    // float h = dht_.readHumidity();
+    // float t = dht_.readTemperature();
 
-    float t = dht_.readTemperature();
+    float h = 0;
+    sensors.requestTemperatures();
+    float t = sensors.getTempCByIndex(0);
 
-    String payload = String("{\"payload\":{\"Temp\":" + String(t) + "}}");
-    custom_network_.mqttPublish(TOPIC, payload.c_str());
-
-    if (isnan(t))
-    {
-        logger_.debugln("Failed to read from DHT sensor!");
+    if (isnan(t) || isnan(h)) {
+        logger_.debugln("Failed to read from DHT sensor - restart!");
+        delay(2 * 1000);
+        return;
     }
+ 
+    custom_network_.wifiConnect();
+
+    h = (int)(h * 100) / 100.0;
+    t = (int)(t * 100) / 100.0;
+
+    String payload = String(
+        "{"
+        "  'n': 'bedroom',"
+        "  't': 'secure',"
+        "  'p': {"
+        "    't':" + String(t) + "," 
+        "    'h':" + String(h) + ""
+        "  }"
+        "}"
+    );
+    payload.replace("'", "\"");
     logger_.debugln(String("payload: " + payload).c_str());
 
-    delay(10 * 60 * 1000);
+    custom_network_.wifiPost(REQUEST_URL, payload.c_str());
+
+    custom_network_.wifiDisconnect();
+    // delay(10 * 60 * 1000);
+    ESP.deepSleep(60 * 1000);
 }
